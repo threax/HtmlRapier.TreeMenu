97bfad26158f84b61d4c46bd8539ebca88bf3127
Andrew Piper <andrew@threax.com>
2021-07-22 16:06:38 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/HtmlRapier.TreeMenu.git
+git push https://github.com/threax/HtmlRapier.TreeMenu.git --tags
+git push https://gitlab.com/threax-software/HtmlRapier.TreeMenu.git
+git push https://gitlab.com/threax-software/HtmlRapier.TreeMenu.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.TreeMenu
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/HtmlRapier.TreeMenu --tags
